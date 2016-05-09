8696fd133fbf5bee7feffac45231158415d489a6
Thomas BRILLARD <brillard.t@sfeir.lu>
2016-05-09 16:28:02 +0200

    env var

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 var authProxy = require('./index');
-var config = require('./config');
+//var config = require('./config');
+
+var config = {
+  firebase_secret : process.env.firebase_secret,
+  client_id :  process.env.client_id
+};
 
 authProxy(config).listen(8081);
