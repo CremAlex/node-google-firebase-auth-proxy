8c59033ce3ceaded46955d5de73acce0eb982212
Thomas BRILLARD <brillard.t@sfeir.lu>
2016-05-19 15:36:36 +0200

    update test.js to take hd

--- test.js
+++ test.js
@@ -2,8 +2,9 @@ var authProxy = require('./index');
 //var config = require('./config');
 
 var config = {
-  firebase_secret : process.env.firebase_secret,
-  client_id :  process.env.client_id
+  firebase_secret   : process.env.firebase_secret,
+  client_id         : process.env.client_id,
+  hd                : process.env.hd || null
 };
 
 authProxy(config).listen(process.env.PORT || 5000);
